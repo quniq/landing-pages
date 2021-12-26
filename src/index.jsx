cca080fe89fbcc7f7f2432dec76db701935ffd5b
lucky <luckyv1@umbc.edu>
2021-12-25 19:47:08 -0500

    new build

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,7 @@ import App from './App';
 
 
 ReactDOM.render(
-<<<<<<< HEAD
   <React.StrictMode>
-=======
-  <HashRouter base="/landing-pages">
->>>>>>> 2ee01cc868c116cd1dd0b25e6f469db3e11658f9
     <App />
   </React.StrictMode>,
   document.getElementById('root')
